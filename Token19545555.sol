3433ea09745241d96372e0f0e37c2d8a88ec5d58
Litecointoken <35626103+waquar213@users.noreply.github.com>
2020-05-05 20:48:39 +0530

    Update Token19545555.sol

--- Token19545555.sol
+++ Token19545555.sol
@@ -1,5 +1,6 @@
 please Don't discuss about any personal details on FIVERR
 
+for etherscan verification i need lots of details like  email or lots of stuff which i dont ask u on fiverr .
 
 Please contact me on Telegram or WhatsApp
 

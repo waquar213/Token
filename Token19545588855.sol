9a89953d150f7ea12e5c5c194fcbc8094d99d649
Litecointoken <35626103+waquar213@users.noreply.github.com>
2020-06-06 09:16:05 +0530

    Update Token19545588855.sol

--- Token19545588855.sol
+++ Token19545588855.sol
@@ -1,9 +1,5 @@
 please Don't discuss about any personal details on FIVERR
 
-for etherscan verification i need lots of details like  email or lots of stuff which i dont ask u on fiverr .
-
 Please contact me on Telegram or WhatsApp
-
-Telegram : @Blood_Space
 
 WhatsApp : +918279388875

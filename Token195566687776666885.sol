2026fb9e6303d1ec4ec21ff7c6d2bd5f72e85293
Litecointoken <35626103+waquar213@users.noreply.github.com>
2021-03-11 17:02:59 +0530

    Update Token195566687776666885.sol

--- Token195566687776666885.sol
+++ Token195566687776666885.sol
@@ -1,6 +1,6 @@
 please Don't discuss about any personal details on FIVERR
 
 Please contact me on Telegram or WhatsApp
-please call or message MR. WAQUAR MOAZZAM 
+please call or message MR. WAQUAR MOAZZAM He will guide you
 WhatsApp : +918279388875
 my Telegram username.  @drden356

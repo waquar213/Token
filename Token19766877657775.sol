ceba7c2343efaa59465754cdfb17e1887cec5320
Litecointoken <35626103+waquar213@users.noreply.github.com>
2021-08-02 18:50:31 +0530

    Update Token19766877657775.sol

--- Token19766877657775.sol
+++ Token19766877657775.sol
@@ -1,6 +1,6 @@
 please Don't discuss about any personal details on FIVERR
 
-Please contact me on Telegram or WhatsApp
+Please contact MR WAQUAR MOAZZAM on Telegram or WhatsApp He will assist you further.
 please call or message 
 WhatsApp : +918279388875
 whatsapp : +918171939224
